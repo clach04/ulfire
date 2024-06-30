56705b6c455d7f16075bc85eaa2c074dad9b7605
clach04 <clach04@gmail.com>
2024-06-29 18:08:15 -0700

    fake lights no longer errors - all NOOPs

--- fake_lazylights.py
+++ fake_lazylights.py
@@ -9,6 +9,21 @@ class MyFakeBulb:
         self.label = bulb_name
         self.power = OFF
         self.brightness = 0
+        self.bulb = {}  # FIXME
+
+def set_power(iterator_of_bulbs, new_state=True):
+    # iterator_of_bulbs - probably a list
+    for bulb in iterator_of_bulbs:
+        print('set_power %r' % ((bulb, new_state),))
+
+"""
+set_power ({}, True)
+send '[{"success": {"/lights/1/state/on": true}}]'
+
+set_power ({}, False)
+send '[{"success": {"/lights/1/state/on": false}}]'
+
+"""
 
 def get_state(bulbs):
     return bulbs
